d8412b6813f09d22b1795ddcf13aba76307675ba
Will Storey <will@summercat.com>
2014-03-29 10:45:53 -0700

    google.tcl: change namespace reference style
    also remove some header comments

--- scripts/google.tcl
+++ scripts/google.tcl
@@ -1,20 +1,4 @@
 #
-# 0.3 - ?
-#  - switch from decode_html to htmlparse::mapEscape
-#  - fix issue with encoding getting ascii
-#  - add !g1 for one result
-#  - strip remaining html from api result
-#
-# 0.2 - May 10 2010
-#  - fix for garbled utf chars in api queries
-#  - added +google channel flag to enable
-#  - strip html from !convert as some formatting may be present
-#  - fix decode_html to convert html utf to hex
-#  - convert <sup></sup> to exponent
-#
-# 0.1 - Some time in April 2010
-#  - Initial release
-#
 # Created Feb 28 2010
 #
 # Requires Tcl 8.5+
@@ -25,7 +9,7 @@ package require http
 package require json
 package require htmlparse
 
-namespace eval google {
+namespace eval ::google {
 	variable useragent_api "Lynx/2.8.8dev.2 libwww-FM/2.14 SSL-MM/1.4.1"
 	variable useragent_convert "Mozilla/5.0 (X11; Linux i686; rv:8.0) Gecko/20100101 Firefox/8.0"
 
@@ -36,18 +20,18 @@ namespace eval google {
 
 	variable api_referer "http://www.egghelp.org"
 
-	signal_add msg_pub "!g" google::search
-	signal_add msg_pub "!google" google::search
-	signal_add msg_pub "!g1" google::search1
-	signal_add msg_pub "!news" google::news
-	signal_add msg_pub "!images" google::images
-	signal_add msg_pub "!convert" google::convert
+	signal_add msg_pub "!g"       ::google::search
+	signal_add msg_pub "!google"  ::google::search
+	signal_add msg_pub "!g1"      ::google::search1
+	signal_add msg_pub "!news"    ::google::news
+	signal_add msg_pub "!images"  ::google::images
+	signal_add msg_pub "!convert" ::google::convert
 
 	settings_add_str "google_enabled_channels" ""
 }
 
 # Query normal html for conversions
-proc google::convert {server nick uhost chan argv} {
+proc ::google::convert {server nick uhost chan argv} {
 	if {![str_in_settings_str google_enabled_channels $chan]} { return }
 
 	if {[string length $argv] == 0} {
@@ -55,15 +39,16 @@ proc google::convert {server nick uhost chan argv} {
 		return
 	}
 
-	http::config -useragent $google::useragent_convert
-	set query [http::formatQuery q $argv]
-	set token [http::geturl ${google::convert_url}?${query} -command "google::convert_callback $server $chan"]
+	::http::config -useragent $::google::useragent_convert
+	set query [::http::formatQuery q $argv]
+	set token [::http::geturl ${::google::convert_url}?${query} \
+		-command "::google::convert_callback $server $chan"]
 }
 
-proc google::convert_callback {server chan token} {
-	set data [http::data $token]
-	set ncode [http::ncode $token]
-	http::cleanup $token
+proc ::google::convert_callback {server chan token} {
+	set data [::http::data $token]
+	set ncode [::http::ncode $token]
+	::http::cleanup $token
 
 	# debug
 	#set fid [open "g-debug.txt" w]
@@ -75,7 +60,7 @@ proc google::convert_callback {server chan token} {
 		return
 	}
 
-	if {[catch {google::convert_parse $data} result]} {
+	if {[catch {::google::convert_parse $data} result]} {
 		putchan $server $chan "Error: $result."
 		return
 	}
@@ -83,11 +68,11 @@ proc google::convert_callback {server chan token} {
 	putchan $server $chan "\002$result\002"
 }
 
-proc google::convert_parse {html} {
-	if {![regexp -- $google::convert_regexp $html -> result]} {
+proc ::google::convert_parse {html} {
+	if {![regexp -- $::google::convert_regexp $html -> result]} {
 		error "Parse error or no result"
 	}
-	set result [htmlparse::mapEscapes $result]
+	set result [::htmlparse::mapEscapes $result]
 	# change <sup>num</sup> to ^num (exponent)
 	set result [regsub -all -- {<sup>(.*?)</sup>} $result {^\1}]
 	# strip rest of html code
@@ -95,33 +80,34 @@ proc google::convert_parse {html} {
 }
 
 # Output for results from api query
-proc google::output {server chan url title content} {
+proc ::google::output {server chan url title content} {
 	regsub -all -- {(?:<b>|</b>)} $title "\002" title
 	regsub -all -- {<.*?>} $title "" title
 	set output "$title @ $url"
-	putchan $server $chan "[htmlparse::mapEscapes $output]"
+	putchan $server $chan "[::htmlparse::mapEscapes $output]"
 }
 
 # Query api
-proc google::api_handler {server chan argv url {num {}}} {
+proc ::google::api_handler {server chan argv url {num {}}} {
 	if {[string length $argv] == 0} {
 		putchan $server $chan "Error: Please supply search terms."
 		return
 	}
-	set query [http::formatQuery v "1.0" q $argv safe off]
-	set headers [list Referer $google::api_referer]
+	set query [::http::formatQuery v "1.0" q $argv safe off]
+	set headers [list Referer $::google::api_referer]
 	if {$num == ""} {
 		set num 4
 	}
 
-	http::config -useragent $google::useragent_api
-	set token [http::geturl ${url}?${query} -headers $headers -method GET -command "google::api_callback $server $chan $num"]
+	::http::config -useragent $::google::useragent_api
+	set token [::http::geturl ${url}?${query} -headers $headers -method GET \
+		-command "::google::api_callback $server $chan $num"]
 }
 
-proc google::api_callback {server chan num token} {
-	set data [http::data $token]
-	set ncode [http::ncode $token]
-	http::cleanup $token
+proc ::google::api_callback {server chan num token} {
+	set data [::http::data $token]
+	set ncode [::http::ncode $token]
+	::http::cleanup $token
 
 	# debug
 	#set fid [open "g-debug.txt" w]
@@ -134,7 +120,7 @@ proc google::api_callback {server chan num token} {
 		return
 	}
 
-	set data [json::json2dict $data]
+	set data [::json::json2dict $data]
 	set response [dict get $data responseData]
 	set results [dict get $response results]
 
@@ -150,46 +136,46 @@ proc google::api_callback {server chan num token} {
 		dict with result {
 			# $language holds lang in news results, doesn't exist in web results
 			if {![info exists language] || $language == "en"} {
-				google::output $server $chan $unescapedUrl $title $content
+				::google::output $server $chan $unescapedUrl $title $content
 			}
 		}
 	}
 }
 
 # Regular API search
-proc google::search {server nick uhost chan argv} {
+proc ::google::search {server nick uhost chan argv} {
 	if {![str_in_settings_str "google_enabled_channels" $chan]} {
 		return
 	}
 
-	google::api_handler $server $chan $argv ${google::api_url}web
+	::google::api_handler $server $chan $argv ${::google::api_url}web
 }
 
 # Regular API search, 1 result
-proc google::search1 {server nick uhost chan argv} {
+proc ::google::search1 {server nick uhost chan argv} {
 	if {![str_in_settings_str "google_enabled_channels" $chan]} {
 		return
 	}
 
-	google::api_handler $server $chan $argv ${google::api_url}web 1
+	::google::api_handler $server $chan $argv ${google::api_url}web 1
 }
 
 # News from API
-proc google::news {server nick uhost chan argv} {
+proc ::google::news {server nick uhost chan argv} {
 	if {![str_in_settings_str "google_enabled_channels" $chan]} {
 		return
 	}
 
-	google::api_handler $server $chan $argv ${google::api_url}news
+	::google::api_handler $server $chan $argv ${::google::api_url}news
 }
 
 # Images from API
-proc google::images {server nick uhost chan argv} {
+proc ::google::images {server nick uhost chan argv} {
 	if {![str_in_settings_str "google_enabled_channels" $chan]} {
 		return
 	}
 
-	google::api_handler $server $chan $argv ${google::api_url}images
+	::google::api_handler $server $chan $argv ${google::api_url}images
 }
 
 irssi_print "google.tcl loaded"
