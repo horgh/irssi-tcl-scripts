0efc51ff9458d8e30186b4530cd99e88d2a6af92
Will Storey <will@summercat.com>
2017-11-26 20:26:46 -0800

    latoc: Update to recognize new page

--- scripts/latoc.tcl
+++ scripts/latoc.tcl
@@ -1,44 +1,26 @@
-#
-# created by horgh
-#
-# Provides some binds to read Yahoo.com futures
-#
+# Provides binds to read Yahoo.com futures
 
 package require http
 
 namespace eval ::latoc {
 	variable user_agent "Lynx/2.8.5rel.1 libwww-FM/2.14 SSL-MM/1.4.1 OpenSSL/0.9.7e"
 
-	variable list_regexp {<tr><td class="first">.*?<td class="last">.*?</td></tr>}
-	variable stock_regexp {<a href="/q\?s=(.*?)">.*?<td class="second name">(.*?)</td>.*?<span id=".*?">(.*?)</span></b> <nobr><span id=".*?">(.*?)</span></nobr>.*?(?:alt="(.*?)">)?? <b style="color.*?;">(.*?)</b>.*?<b style="color.*?;"> \((.*?)\)</b>}
+	variable list_regexp {<tr class="data-row.*?".*?</a></td></tr>}
+	variable stock_regexp {<td class="data-col0.*>(.*)</a></td><td class="data-col1.*>(.*)</td><td class="data-col2.*>(.*)</td><td class="data-col3.*>(.*)</td><td class="data-col4.*>(.*)<!-- /react-text --></span></td><td class="data-col5.*>(.*)<!-- /react-text --></span></td><td class="data-col6.*>(.*)</td><td class="data-col7.*>(.*)</td><td class="data-col8.*"}
 
-	# any names matching this pattern are not shown
-	variable skip_regexp {(5000 oz)|(100 oz)}
-
-	variable commodities [list energy metals grains livestock softs]
-	variable energy_futures "http://finance.yahoo.com/futures?t=energy"
-	variable commodities_url "http://finance.yahoo.com/futures?t="
+	variable url "https://finance.yahoo.com/commodities?ltr=1"
 
 	signal_add msg_pub "!oil"    ::latoc::oil_handler
 	signal_add msg_pub "!gold"   ::latoc::gold_handler
-	signal_add msg_pub "!c"      ::latoc::commodity_handler
 	signal_add msg_pub "!silver" ::latoc::silver_handler
-	signal_add msg_pub "!url"    ::latoc::url_handler
 
 	settings_add_str "latoc_enabled_channels" ""
 }
 
-# fetch lines from given commodity type (url) and only return lines that
-# match the given pattern (regexp) to Name (optional)
-# return list of lines, each a stock
-proc ::latoc::fetch {type pattern server chan} {
+proc ::latoc::fetch {} {
 	::http::config -useragent $::latoc::user_agent
-	set token [::http::geturl ${::latoc::commodities_url}${type} -timeout 20000 -command "::latoc::output [list $pattern] $server $chan"]
-}
+	set token [::http::geturl $::latoc::url -timeout 20000]
 
-# output is the callback proc from ::http::geturl
-proc ::latoc::output {pattern server chan token} {
-	# status tells us overall request status.
 	set status [::http::status $token]
 	if {$status != "ok"} {
 		set http_error [::http::error $token]
@@ -46,78 +28,81 @@ proc ::latoc::output {pattern server chan token} {
 		::http::cleanup $token
 		return
 	}
-	# we need HTTP 200.
+
 	set ncode [::http::ncode $token]
 	if {$ncode != 200} {
-		# code is something like "HTTP/1.1 500 Internal Server Error"
-		# NOTE: don't output data here as it could be a large HTML document.
 		set code [::http::code $token]
 		putchan $server $chan "HTTP error: $ncode: $code"
 		::http::cleanup $token
 		return
 	}
-	# everything seems OK, move on to looking at the body.
+
 	set data [::http::data $token]
 	::http::cleanup $token
+
+	return $data
+}
+
+proc ::latoc::parse {data} {
 	set lines []
 	foreach stock [regexp -all -inline -- $::latoc::list_regexp $data] {
-		regexp $::latoc::stock_regexp $stock -> symbol name price last direction change percent
-		if {[regexp -- $pattern $name]} {
-			if {[regexp -- $::latoc::skip_regexp $name]} {
-				continue
-			}
-			lappend lines [::latoc::format $name $price $last $direction $change $percent]
+		regexp $::latoc::stock_regexp $stock -> symbol name price last change percent volume interest
+		set direction none
+		if {$change < 0} {
+			set direction Down
 		}
+		if {$change > 0} {
+			set direction Up
+		}
+		lappend lines [::latoc::format $name $price $last $direction $change $percent]
 	}
-	if {[llength $lines] == 0} {
-		lappend lines "No results."
-	}
+
+	return $lines
+}
+
+proc ::latoc::output {server chan lines symbol_pattern} {
 	foreach line $lines {
+		if {![regexp -- $symbol_pattern $line]} {
+			continue
+		}
 		putchan $server $chan "$line"
 	}
 }
 
-proc ::latoc::url_handler {server nick uhost chan argv} {
-	if {![str_in_settings_str "latoc_enabled_channels" $chan]} { return }
-	putchan $server $chan "$::latoc::commodities_url"
-}
-
-proc ::latoc::commodity_handler {server nick uhost chan argv} {
-	if {![str_in_settings_str "latoc_enabled_channels" $chan]} { return }
-	if {[lsearch $::latoc::commodities $argv] == -1} {
-		putchan $server $chan "Valid commodities are: $::latoc::commodities"
-		return
-	}
-	::latoc::fetch $argv "." $server $chan
-}
-
 proc ::latoc::oil_handler {server nick uhost chan argv} {
 	if {![str_in_settings_str "latoc_enabled_channels" $chan]} { return }
-	::latoc::fetch "energy" "Crude Oil" $server $chan
+
+	set data [::latoc::fetch]
+	set lines [::latoc::parse $data]
+	::latoc::output $server $chan $lines {Crude Oil}
 }
 
 proc ::latoc::gold_handler {server nick uhost chan argv} {
 	if {![str_in_settings_str "latoc_enabled_channels" $chan]} { return }
-	::latoc::fetch "metals" "Gold" $server $chan
+
+	set data [::latoc::fetch]
+	set lines [::latoc::parse $data]
+	::latoc::output $server $chan $lines {Gold}
 }
 
 proc ::latoc::silver_handler {server nick uhost chan argv} {
 	if {![str_in_settings_str "latoc_enabled_channels" $chan]} { return }
-	::latoc::fetch "metals" "Silver" $server $chan
+
+	set data [::latoc::fetch]
+	set lines [::latoc::parse $data]
+	::latoc::output $server $chan $lines {Silver}
 }
 
 proc ::latoc::format {name price last direction change percent} {
-	# this cuts off the Jun 09 part from Crude Oil Jun 09
-	#set name [lrange $name 0 [expr [llength $name]-3]]
 	return "$name: \00310$price [::latoc::colour $direction $change] [::latoc::colour $direction $percent]\003 $last"
 }
 
 proc ::latoc::colour {direction value} {
 	if {[string match "Down" $direction]} {
-		return \00304-$value\017
+		return \00304$value\017
 	}
 	if {[string match "Up" $direction]} {
-		return \00309+$value\017
+		return \00309$value\017
 	}
 	return $value
 }
